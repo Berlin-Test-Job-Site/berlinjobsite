459e64fdf4eed2f6f16ecffaba558dd2064500fc
Edward Tanguay <edward@tanguay.info>
2024-02-26 14:09:47 +0100

    Edward: fetched and displayed jobs
    * added url
    
    * jobs in welcome page
    
    * finished

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { IJob } from "../interfaces";
+import axios from "axios";
+
+const url = "http://localhost:4206";
+
 export const PageWelcome = () => {
+	const [jobs, setJobs] = useState<IJob[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${url}/jobs`);
+			const _jobs = response.data;
+			setJobs(_jobs);
+		})();
+	});
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2 className="mb-4 text-xl">There are {jobs.length} jobs:</h2>
+
+			<ul>
+				<>
+					{jobs.map((job) => {
+						return <li key={job.id}>{job.title}</li>;
+					})}
+				</>
+			</ul>
+		</>
+	);
+};
