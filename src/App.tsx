385bb06b94ce25ec7cc4bbbcf2862af997f56ebe
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-26 14:02:19 +0100

    jobs in welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 
-const url = "http://localhost:4206";
-
 function App() {
 	return (
 		<>
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import { IJob } from "../interfaces";
+import axios from "axios";
+
+const url = "http://localhost:4206";
+
 export const PageWelcome = () => {
+	const [jobs, setJobs] = useState<IJob[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${url}/jobs`);
+			const _jobs = response.data;
+			setJobs(_jobs);
+		})();
+	});
+
 	return (
-		<p>This is the welcome page.</p>
+		<h2>There are {jobs.length} jobs:</h2>
 	)
 }
