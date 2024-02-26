cfe119d25ec7fd2dcadcf1517e492caf000bf08b
Selman <selman.ciftci.coding@gmail.com>
2024-02-26 14:29:31 +0100

    Selman: added telephone number to welcome page

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -19,6 +19,10 @@ export const PageWelcome = () => {
 		<>
 			<p className="text-blue-900 mb-4 text-xl">Welcome to this site, we are glad you are here.</p>
 			<h2 className="mb-4 text-xl">There are {jobs.length} jobs:</h2>
+			<p className="mb-4">
+				This is Berlin Jobs Site, you can contact us with following
+				number: <span className="font-bold">+49 00123 512398</span>{" "}
+			</p>
 
 			<ul>
 				<>
